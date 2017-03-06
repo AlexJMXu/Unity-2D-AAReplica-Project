9d6e2c9e1c3abe548781ba4dcb55f2391a959c69
Alexander Xu <ajm.xu@outlook.com>
2017-03-06 23:31:51 +0000

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	private bool gameHasEnded = false;
+
+	public static GameManager instance;
+
+	public Rotator rotator;
+	public Spawner spawner;
+
+	public Animator animator;
+
+	void Awake() {
+		instance = this;
+	}
+
+	public void EndGame() {
+		if (!gameHasEnded) {
+			rotator.enabled = false;
+			spawner.enabled = false;
+			animator.SetTrigger("EndGame");
+			gameHasEnded = true;
+		}
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
